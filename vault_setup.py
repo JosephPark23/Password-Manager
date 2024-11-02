3bbe05c584b263cdc5a2041e3a34a400d86d9063
JosephPark23 <joseph0701p@gmail.com>
2024-11-02 09:33:55 -0400

    Update vault_setup file

--- vault_setup.py
+++ vault_setup.py
@@ -1,43 +1,53 @@
 # dependencies
-import cryptography
-from cryptography.fernet import Fernet
 import csv
-import pbkdf2
+from cryptography.fernet import Fernet
+import pbkdf2 as pb
 import hashlib
 import os
+
 
 class Vault:
 
     def __init__(self):
         self.master_password = ""
+        self.salt = b''
 
     def create_data_file(self):
-        with open('chase.csv', 'x', newline='') as csvfile:
+        # create a data template
+        with open('.chase/.csv', 'x', newline='') as csvfile:
             vault_writer = csv.writer(csvfile, delimiter=" ", quotechar="|", )
             vault_writer.writerow(['Username', 'URL', 'Salt', 'Password'])
-        csvfile.close()
 
     def set_master_password(self):
+        # get master password from user
         self.master_password = input("Enter your master password: ")
+        self.master_password = bytes(self.master_password, "utf-8")
+
+        # derive key from password using pbkdf2
+        self.salt = os.urandom(32)
+
+        with open('.chase/.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["Master Password", "None", self.salt, self.master_password])
 
 
     def encrypt_vault_file(self):
-        # generate a key
-        key = Fernet.generate_key()
-        fernet = Fernet(key)
-
         # read plaintext contents and encrypt using Fernet
-        with open("chase.csv", "rb") as csvfile:
+        fernet = Fernet(self.derived_key)
+
+        with open(".chase/.csv", "rb") as csvfile:
             contents = csvfile.read()
         encrypted_contents = fernet.encrypt(contents)
 
         # write the encrypted contents back into the file
-        with open("chase.csv", "wb") as csvfile:
+        with open(".chase/.csv", "wb") as csvfile:
             csvfile.write(encrypted_contents)
 
         # save the key in a different file
         with open("way.key", "wb") as keyfile:
-            keyfile.write(key)
+            keyfile.write(self.derived_key)
+
+
 
 
 
